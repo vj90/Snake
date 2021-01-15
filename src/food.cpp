96e88e7915862c69d4318993dfbfa98975d58dca
Vasundhara Jain <vasundhara.jain@daimler.com>
2021-01-15 20:16:37 +0100

    Introduce food class

---
+++ src/food.cpp
@@ -0,0 +1,16 @@
+#include "food.h"
+
+Food::Food(const std::size_t grid_width, const std::size_t grid_height)
+    : _engine(_dev()),
+      _random_w(0, static_cast<int>(grid_width) - 1),
+      _random_h(0, static_cast<int>(grid_height) - 1) {}
+
+void Food::Generate() {
+  this->x = _random_w(_engine);
+  this->y = _random_h(_engine);
+}
+
+void Food::Render() {
+  _parent_renderer_handle->SetColor(0xFF, 0xCC, 0x00, 0xFF);
+  _parent_renderer_handle->FillBlock(this->x, this->y);
+}
---
+++ src/food.h
@@ -0,0 +1,22 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include <random>
+
+#include "SDL.h"
+#include "game_component.h"
+
+class Food : public GameComponent, public SDL_Point {
+ public:
+  Food(const std::size_t grid_width, const std::size_t grid_height);
+  void Generate();
+  void Render();
+
+ private:
+  std::random_device _dev;
+  std::mt19937 _engine;
+  std::uniform_int_distribution<int> _random_w;
+  std::uniform_int_distribution<int> _random_h;
+};
+
+#endif
